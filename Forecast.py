128045cdfa0cac95eb18a4c2ffc16b7df2097bd5
Siyi Xu <emmaxu2005@gmail.com>
2024-02-24 16:48:28 +0800

    all

--- Forecast.py
+++ Forecast.py
@@ -3,13 +3,10 @@ import numpy as np
 import random
 
 # Constants
-WEEKMINS = 7 * 24 * 60 * 60
+WEEKMINS = 7 * 24 * 60 
 chemicalLife = 500
 
-# Parameter predefined
-reclaimEfficiency = 0.09
-batchLifes = [7, 8]
-loadSizes = [38, 50]
+
 
 
 
@@ -21,21 +18,22 @@ print(demand)
 class Tank: 
     def __init__(self, size):
         self.size = size
-        self.chemicalLife = 500
-        self.batchLife = 1
+        self.remainBatchLife = 1
         
     def __str__(self):
         return f"tank sized {self.size}L"
     
     def replenish(self, reclaimEfficiency):
-        self.batchLife = 1
+        self.remainBatchLife = 1
         return self.size * (1- reclaimEfficiency)
+    def processOneBatch(self, product):
+        return 0
 
 class Product:
     def __init__(self, id, batchLife, loadSize):
         self.id = id
         self.remain = 0
-        self.batchLife = 1 / batchLife
+        self.costPerBatch = 1 / batchLife
         self.loadSize = loadSize
         
     def __str__(self):
@@ -58,21 +56,28 @@ def forecast(tankSize, productNum, batchLifes, loadSizes, reclaimEfficiency, dem
     predictionRange = WEEKMINS * weeks
     usageResult = []
     
-    for i in range(productNum):  ##initialization
+    for i in range(productNum):  ##initialization products
         new = Product(i, batchLifes[i], loadSizes[i])
         products.append(new)
     
     
         
-    weekUsage = 0
-    for time in range(0, predictionRange): ## predictionRange in min
-        weekIndex = 0
+    weekUsage = 0 ##initialize week info
+    weekIndex = 0
+    weekDemand = []
+    for i in range(productNum):
+        weekDemand.append(demand.iloc[i, weekIndex])   
         
-        if not time % chemicalLife: ## replenish when chemical life reached
+    for time in range(0, predictionRange): ## predictionRange in min
+        if not time % chemicalLife: ## replenish when chemical life reached replenish at start
             weekUsage += tank.replenish(reclaimEfficiency(time))
-            
-        if not time % WEEKMINS: ## initiate new week
-            weekIndex = time // WEEKMINS
+        
+        
+        
+        
+        
+        if not (time + 1) % WEEKMINS and weekIndex < weeks - 1: ## initiate new week after one week
+            weekIndex += 1
             print(weekIndex)
             weekDemand = []
             for i in range(productNum):
@@ -85,7 +90,14 @@ def forecast(tankSize, productNum, batchLifes, loadSizes, reclaimEfficiency, dem
             
         
     return usageResult
-    
 
+
+
+#Test
+
+# Parameter predefined
+reclaimEfficiency = 0.09
+batchLifes = [7, 8]
+loadSizes = [38, 50]
 # Print Results
 print(forecast(80, 2, batchLifes,loadSizes, reclaimEfficiency, demand, 10))
