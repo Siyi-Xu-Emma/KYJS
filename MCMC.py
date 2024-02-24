759e596f1681c95cfb9984c638de5b15d35d92c7
Siyi Xu <emmaxu2005@gmail.com>
2024-02-24 21:19:33 +0800

    Update MCMC.py

--- MCMC.py
+++ MCMC.py
@@ -1,31 +1,37 @@
 import pandas as pd
 import numpy as np
 import random
+SIMULATION_RANGE = 168
+TOTAL_STEPS_PER_LOT = 5
 
 class Recipe:
     def __str__(self):
         return f"Recipe{self.id}"
     
     def __init__(self, id, time):
-        self.time = time
+        self.remainTime = time
         self.id = id
     
     def getId(self):
         return self.id
     
-    def getTime(self):
-        return self.time
+    def getRemainTime(self):
+        return self.remainTime
+    
+    def cutRemainTime(self):
+        self.remainTime -= 1
+
 class Lot:
     def __str__(self):
         return f"Lot{self.id}"
     
     def __init__(self, id, recipe):  
         self.id = id
-        self.step = 1
+        self.step = TOTAL_STEPS_PER_LOT
         self.recipe = recipe
         
     def newStep(self, newRecipe):
-        self.step += 1
+        self.step -= 1
         self.recipe = newRecipe
         
     def getId(self):
@@ -33,6 +39,7 @@ class Lot:
     
     def getStep(self):
         return self.step
+
 class State:
     def __init__(self, id):
         self.id = id
@@ -45,26 +52,28 @@ class Idle(State):
         super().__init__(0)
 
 class Processing(State):
-    def __init__(self):
-        super().__init__(1)  
+    def __init__(self, lot, recipe):
+        super().__init__(1) 
+        self.lot = lot
+        self.recipe = recipe 
  
 class Switching(State):
-    def __init__(self):
-        super().__init__(2)          
+    def __init__(self, startRecipe, endRecipe):
+        super().__init__(2) 
+         
 class Equipment:
     def __str__(self):
         return f"Equipment{self.id}"
     
     def __init__(self, id):
         self.id = id
-        self.recipe = None
         self.state = Idle()##Idle: 0 processing: 1, switching: 2
     
     def getId(self):
         return self.id
-    
-    def changeState(self, state):
-        self.state = state
+
+    def changeState(self, newState):
+        self.state = newState
     
     def getStateId(self):
         return self.state.getId()
@@ -73,15 +82,38 @@ class Equipment:
         
 
  
-### Initialization
-startingEquipmentList = [Equipment(0), Equipment(1), Equipment(2)]
+
 
 def simulation(startingEquipmentList):
-    pass
+    profit = 0
+    sampleStatusList = []
+    currentStatusList = startingEquipmentList
+    
+    
+    ### Start simulation
+    for i in range(SIMULATION_RANGE):
+        for equipment in currentStatusList:
+            if equipment.getStateId() == 0: # is idle
+                
+            
+        
+    return (profit, sampleStatusList)
+    
+    
     
 
 
 
+### Initialization
+startingEquipmentList = [Equipment(0), Equipment(1), Equipment(2)]
 
+for simulationTime in range(10086):
+    maxProfit = 0
+    resultPair = simulation(startingEquipmentList)
+    statusList = []
+    if resultPair[0] > maxProfit:
+        maxProfit = resultPair[0]
+        statusList = resultPair[1]
 
-print(simulation(startingEquipmentList))
+print("Maximum Profit:\n", maxProfit, "StatusList:\n", statusList)
+
