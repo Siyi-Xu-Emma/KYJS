fc705799205a6047301d2eb4eb07bbabec4d518a
Siyi Xu <emmaxu2005@gmail.com>
2024-02-24 19:55:50 +0800

    Create MCMC.py

---
+++ MCMC.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+import random
+
+from Forecast import *
+class Recipe:
+    def __init__(self, id, ):
+        self.
+        
+class Processor:
+    def __init__(self):
+        self.recipe = None
+        
+### Initialization
+currentState = [None, None, None]
+def simulation(currentState):
+    
