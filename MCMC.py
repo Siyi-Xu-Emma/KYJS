d32f78dfe82877f188b443445cff13d97a190775
Siyi Xu <emmaxu2005@gmail.com>
2024-02-24 20:06:31 +0800

    Update MCMC.py

--- MCMC.py
+++ MCMC.py
@@ -2,16 +2,47 @@ import pandas as pd
 import numpy as np
 import random
 
-from Forecast import *
 class Recipe:
-    def __init__(self, id, ):
-        self.
-        
-class Processor:
-    def __init__(self):
+    def __str__(self):
+        return f"Recipe{self.id}"
+    
+    def __init__(self, id, time):
+        self.time = time
+        self.id = id
+    
+    def getId(self):
+        return self.id
+    
+    def getTime(self):
+        return self.time
+ 
+ 
+ 
+class Lot:
+    def __str__(self):
+        return f"Lot{self.id}"
+    
+    def __init__(self, id):  
+        self.id = id
+    
+         
+class Equipment:
+    def __str__(self):
+        return f"Equipment{self.id}"
+    
+    def __init__(self, id):
         self.recipe = None
         
+
+ 
 ### Initialization
-currentState = [None, None, None]
+currentState = [Equipment(0), Equipment(1), Equipment(2)]
 def simulation(currentState):
+    pass
     
+
+
+
+
+
+print(simulation(currentState))
