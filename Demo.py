49fd156f5af84ceea7f1bfc4542d2a941958b788
Siyi Xu <emmaxu2005@gmail.com>
2024-02-24 14:43:51 +0800

    datasets

--- Demo.py
+++ Demo.py
@@ -1,5 +1,14 @@
 import pandas as pd
 import numpy as np
 
-def forecast():
+# Constants
+WEEKHRS = 10080
+chemicalLife = 500
+
+# Parameter predefined
+reclaimEfficiency = 0.09
+batchLife = [7, 8]
+
+   
+def forecast(batchLife, ):
     return 
