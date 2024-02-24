2695ccb097b7f0fbd34d4bf548cad94a9d94806e
Siyi Xu <emmaxu2005@gmail.com>
2024-02-24 20:37:19 +0800

    summary

--- MCMC.py
+++ MCMC.py
@@ -33,7 +33,24 @@ class Lot:
     
     def getStep(self):
         return self.step
-           
+class State:
+    def __init__(self, id):
+        self.id = id
+    
+    def getId(self):
+        return self.id
+   
+class Idle(State):
+    def __init__(self):
+        super().__init__(0)
+
+class Processing(State):
+    def __init__(self):
+        super().__init__(1)  
+ 
+class Switching(State):
+    def __init__(self):
+        super().__init__(2)          
 class Equipment:
     def __str__(self):
         return f"Equipment{self.id}"
@@ -41,18 +58,25 @@ class Equipment:
     def __init__(self, id):
         self.id = id
         self.recipe = None
-        self.state = 0 ##Idle: 0 processing: 1, switching: 2
+        self.state = Idle()##Idle: 0 processing: 1, switching: 2
     
     def getId(self):
         return self.id
+    
+    def changeState(self, state):
+        self.state = state
+    
+    def getStateId(self):
+        return self.state.getId()
+        
     
         
 
  
 ### Initialization
-currentState = [Equipment(0), Equipment(1), Equipment(2)]
+startingEquipmentList = [Equipment(0), Equipment(1), Equipment(2)]
 
-def simulation(currentState):
+def simulation(startingEquipmentList):
     pass
     
 
@@ -60,4 +84,4 @@ def simulation(currentState):
 
 
 
-print(simulation(currentState))
+print(simulation(startingEquipmentList))
