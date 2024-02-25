9fde9a664be3ce03075cd35bc19f1315bc9a1dd8
Siyi Xu <emmaxu2005@gmail.com>
2024-02-25 18:59:42 +0800

    Update MarkovDemo.py

--- MarkovDemo.py
+++ MarkovDemo.py
@@ -6,6 +6,7 @@ TOTAL_STEPS_PER_LOT = 5
 SWITCH_TIME = pd.read_csv("switchTime.csv")
 RECIPE_COST = pd.read_csv("recipeCost.csv")
 equipmentData = ([0, 1, 3, 4],[1, 2, 4],[0, 2, 3])
+stepData = ([], [], [], [],[])
 MINIMUM_POSSIBILITY = 0.01
 NORMAL_POSSIBILITY = 0.5
 MAXIMUM_POSSIBILITY = 0.9
@@ -44,14 +45,12 @@ class Lot: ### Init every lot with a recipe and then can change with every steps
     
     def getRecipe(self):
         return self.recipe
-
 class State:
     def __init__(self, id):
         self.id = id
     
     def getId(self):
         return self.id
-   
 class Idle(State):
     def __init__(self):
         super().__init__(0)
@@ -59,7 +58,9 @@ class Idle(State):
     
     def cutRemainingTime(self):
         self.remainingTime -1
-
+    
+    def targetLot(self):
+        return -1
 class Processing(State):
     def __init__(self, lot):
         super().__init__(1) 
@@ -74,7 +75,9 @@ class Processing(State):
     
     def cutRemainingTime(self):
         self.remainingTime -= 1
- 
+    
+    def targetLot(self):
+        return self.lot.getId()
 class Switching(State):
     def __init__(self, startLot, endLot):
         super().__init__(2) 
@@ -91,8 +94,11 @@ class Switching(State):
         self.remainingTime -= 1
         
     def getEndLot(self):
-        return self.endLot   
+        return self.endLot  
     
+    def targetLot(self):
+        return self.endLot.getId()
+      
 class Equipment:
     def __str__(self):
         return f"Equipment{self.id}"
@@ -117,13 +123,15 @@ def convert(currentEquipmentList):
     result = []
     for equipment in currentEquipmentList:
         if equipment.getStateId() == 0:
-            result.append(['idle', 'idle', 'idle'])
+            result.append(['idle'] * 3)
         if equipment.getStateId() == 1:
-            result.append()
-
-        
+            result.append([equipment.getState().getProcessedLot().getId(), equipment.getState().getRecipe().getId(), equipment.getState().getProcessedLot().getStep()])
+        if equipment.getStateId() == 2:
+            result.append(['switch'] * 3)
 
 def choose_with_probability(choices, probabilities): ### Choose one of the choices according to their probabilities.
+    if not choices:
+        return None
     if len(choices) != len(probabilities):
         raise ValueError("Length of choices and probabilities should be the same.")
     # Normalize probabilities to sum up to 1
@@ -133,6 +141,23 @@ def choose_with_probability(choices, probabilities): ### Choose one of the choic
     choice = random.choices(choices, weights=normalized_probabilities, k=1)[0]
     return choice 
 
+def isConflicting(chosenEvents):
+    count = 0
+    eventIdList = []
+    for id in len(chosenEvents):
+        if chosenEvents[id]:
+            count += 1
+            eventIdList.append(id)
+    if count == 3: ### three new events chosen
+        for state in chosenEvents:
+            if state
+    elif count == 2:
+        x = chosenEvents[eventIdList[0]]
+        y = chosenEvents[eventIdList[1]]
+        if x.targetLot
+    else:
+        return True
+        
 def simulation(startingEquipmentList):
     profit = 0
     x = 0
@@ -185,8 +210,15 @@ def simulation(startingEquipmentList):
                     newState = Processing(equipment.getState().getEndLot()) 
                     equipment.changeState(newState)
         # update
-        if all(map(lambda x: ))    
+        events = list(eventsDict.values()) 
+        # a list of 3 lists, each list containing two lists: new states and occuring weightages
+        ## eg: [[[Processing@0000000F, Switching@0000000A], [2, 1]], [], []]
+        chosenevents = list(map(lambda x: choose_with_probability(x[0], x[1])))
+        while(isConflicting(chosenevents)):
+            
+        
         sample.append(convert(currentEquipmentList))
+        
     return (profit, x, y, z, sample)
     
     
